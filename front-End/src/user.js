4754b07d5def74a16a4884a6a668b9552455c1ee
phamthehieu <111071418+phamthehieu@users.noreply.github.com>
2023-02-03 09:29:49 +0700

    edit database master

--- front-End/src/user.js
+++ front-End/src/user.js
@@ -1,5 +1,5 @@
-showHome()
-function showHome() {
+
+function showTransaction() {
     $("#body").html(`
      <!-- fixed-top-->
     <nav class="header-navbar navbar-expand-md navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-light navbar-bg-color">
@@ -313,4 +313,7 @@ function showHome() {
           </script> <a class="text-bold-800 grey darken-2" href="https://themeselection.com/" target="_blank">ThemeSelection </a>, All rights reserved. </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span></p>
     </footer>
     `)
+}
+function showWallet() {
+    $("#body").html(``)
 }
